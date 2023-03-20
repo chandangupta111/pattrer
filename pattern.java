46551478518b6cc6e0ae570a18a681c40343cfaa
Chandan Kumar <chandanguptabasadhi318@gmail.com>
2023-03-20 07:37:43 -0700

    MyPattren

---
+++ pattern.java
@@ -0,0 +1,21 @@
+package chandan;
+import java.util.Scanner;
+
+
+public class pattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i = 1; i<=n; i++) {
+		System.out.println();
+		}
+	    	for(int j=1; j<=n; j++) {
+				System.out.print("*");
+			}
+	}
+	
+	
+
+}
